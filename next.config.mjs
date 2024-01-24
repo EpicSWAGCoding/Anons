caaa9c36eaa7a1e163867121198ab43a117b059d
EpicSWAGCoding <EpicSWAG123@yandex.com>
2024-01-24 18:59:43 +0300

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      domains: ['utfs.io'],
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'utfs.io',
-          port: ''
-        }
-      ]
-    }
-  }
-  
-  module.exports = nextConfig
+const nextConfig = {};
+
+export default nextConfig;
